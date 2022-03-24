0c057198526cc40c179540d6a471a24c4fb858fb
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2022-03-24 22:43:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = TeleBot('5233453489:AAFiUt9BL9gzD-7lU3pwVsPFlY_y2HFZ158')
 @bot.message_handler(content_types=["new_chat_members"])
 def new_member(message):
     name = message.new_chat_members[0].first_name 
-    bot.send_message(message.chat.id, f"Привіт, {name}!\n*Вітаємо Вас в групі для спілкування людей з різних міст України!\nХОЧЕТЕ ЗНАЙТИ ЗЕМЛЯКІВ?\nТОДІ ЖМІТЬ ТУТ:\n https://t.me/c/1541405527/436203\nПравила чату викликаються по команді  /rules* \nРозробник: @ViacheslavUkraine \nПишіть мені якщо у вас є пропозиція!" parse_mode= "Markdown")
+    bot.send_message(message.chat.id, f"*Привіт, {name}!\nВітаємо Вас в групі для спілкування людей з різних міст України!\nХОЧЕТЕ ЗНАЙТИ ЗЕМЛЯКІВ?\nТОДІ ЖМІТЬ ТУТ:\n https://t.me/c/1541405527/436203\nПравила чату викликаються по команді  /rules*", "\nРозробник: @ViacheslavUkraine \nПишіть мені якщо у вас є пропозиція!" parse_mode= "Markdown")
 
 @bot.message_handler(content_types = ['new_chat_members', 'left_chat_member'])
 def delete(message):
