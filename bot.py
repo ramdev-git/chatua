e8a3b3a65aafeade058fa8981c9bd9f91f74151b
Ram2020 <65360179+ramdev-git@users.noreply.github.com>
2022-03-31 19:47:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-from telebot import TeleBot 
+from telebot import TeleBot
+import time
 
 bot = TeleBot('5233453489:AAFiUt9BL9gzD-7lU3pwVsPFlY_y2HFZ158')
 
@@ -7,7 +8,7 @@ bot = TeleBot('5233453489:AAFiUt9BL9gzD-7lU3pwVsPFlY_y2HFZ158')
 def new_member(message):
     name = message.new_chat_members[0].first_name 
     bot.send_message(message.chat.id, f"*Привіт, {name}!\nВітаємо Вас в групі для спілкування людей з різних міст України!\nХОЧЕТЕ ЗНАЙТИ ЗЕМЛЯКІВ?\nТОДІ ЖМІТЬ ТУТ:\n https://t.me/c/1541405527/436203\nПравила чату викликаються по команді  /rules*\nРозробник: @ViacheslavUkraine \nПишіть мені якщо у вас є пропозиція!", parse_mode= "Markdown")
-
+    
 @bot.message_handler(content_types = ['new_chat_members', 'left_chat_member'])
 def delete(message):
     bot.delete_message(message.chat.id, message.message_id) 
@@ -20,7 +21,33 @@ def start(m, res=False):
 
 @bot.message_handler(commands=["rules"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, )
+    bot.send_message(m.chat.id, '''
+ПРАВИЛА ЧАТА.\n
+
+Вам 18+ і Ви з України (громадянство, або місце народження, або проживання)
+забороняється\n
+
+1. Неповажне ставлення до учасників, образи.\n
+
+2. Ненормативна лексика, в тому числі у переданому контенті.\n
+
+3. Порнографія, в тому числі гіф і стікери порнографічного характеру.\n
+
+4. Дискримінація за національною ознакою, статі, орієнтації, релігійними та політичними переконаннями.\n
+
+5. Пропаганда наркотичних речовин, включаючи легкі.\n
+
+6. Завантаження шокуючого контенту.\n
+
+7. Непогоджена з адміністрацією реклама будь-якого характеру, включаючи рекламу інтернет ресурсів, аналогічних груп, інтім.услуг і т.д.\n
+
+8. Видалення і блокування учасників виконується на розсуд адміністрації.\n
+
+
+
+СПІЛКУВАННЯ В ЧАТІ ВІДБУВАЄТЬСЯ  БУДЬ-ЯКОЮ ЗРУЧНОЮ ДЛЯ УЧАСНИКІВ МОВОЮ\n
+
+Приємного спілкування!''' )
 
    
 
